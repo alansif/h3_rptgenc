773434c715fce61883bbe09a6f32ee0404d21350
alansif <alansif@sina.com>
2019-09-07 22:34:38 +0800

    go on

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,115 @@
 <template>
 	<v-stepper non-linear>
 		<v-stepper-header>
-			<v-stepper-step :complete="myFiles1.length>0" step="1" editable>病历</v-stepper-step><v-divider/>
-			<v-stepper-step :complete="myFiles2.length>0" step="2" editable>内镜报告</v-stepper-step><v-divider/>
-			<v-stepper-step :complete="myFiles3.length>0" step="3" editable>病理报告</v-stepper-step><v-divider/>
-			<v-stepper-step :complete="myFiles4.length>0" step="4" editable>化验报告</v-stepper-step><v-divider/>
-			<v-stepper-step :complete="myFiles5.length>0" step="5" editable>心电图</v-stepper-step><v-divider/>
-			<v-stepper-step :complete="true" step="6" editable>生成</v-stepper-step>
+			<v-stepper-step :complete="true" step="0" editable>基本信息</v-stepper-step><v-divider/>
+			<v-stepper-step :complete="hasfile1" step="1" :editable="pedscp.length>0">病历</v-stepper-step><v-divider/>
+			<v-stepper-step :complete="hasfile2" step="2" :editable="pedscp.length>0">内镜报告</v-stepper-step><v-divider/>
+			<v-stepper-step :complete="hasfile3" step="3" :editable="pedscp.length>0">病理报告</v-stepper-step><v-divider/>
+			<v-stepper-step :complete="hasfile4" step="4" :editable="pedscp.length>0">化验报告</v-stepper-step><v-divider/>
+			<v-stepper-step :complete="hasfile5" step="5" :editable="pedscp.length>0">心电图</v-stepper-step><v-divider/>
+			<v-stepper-step :complete="pedscp.length>0" step="6" :editable="pedscp.length>0">生成</v-stepper-step>
 		</v-stepper-header>
+		<v-stepper-items>
+			<v-stepper-content step="0">
+				<v-card max-width="400" class="mx-auto pa-4" outlined>
+					<v-card-text>
+						<v-form>
+							<v-text-field label="病案号" outlined append-icon="search" v-model="pid"
+								@click:append="search" @keyup.enter.native="search" @input="clearall"/>
+							<v-text-field label="姓名" readonly v-model="pname"></v-text-field>
+							<v-text-field label="性别" readonly v-model="psex"></v-text-field>
+							<v-text-field label="检查日期" readonly v-model="pexamdate"></v-text-field>
+							<v-select label="检查项目" chips multiple readonly append-icon class="tag-input"
+								:items="['无痛胃镜','无痛肠镜']" v-model="pedscp">
+							</v-select>
+						</v-form>
+					</v-card-text>
+				</v-card>
+			</v-stepper-content>
+		</v-stepper-items>
 		<v-stepper-items>
 			<v-stepper-content step="1">
 				<FilePond
+					ref="fp1"
 					label-idle="拖放病历记录到此处..."
 					:allow-multiple="true"
 					accepted-file-types="image/jpeg, image/png"
 					:files="myFiles1"
+					:imagePreviewMaxHeight="512"
+					name="files"
+					server="/api/upload"
+					@updatefiles="updatefiles"
 				/>
 			</v-stepper-content>
 		</v-stepper-items>
 		<v-stepper-items>
 			<v-stepper-content step="2">
 				<FilePond
+					ref="fp2"
 					label-idle="拖放内镜报告到此处..."
 					:allow-multiple="true"
 					accepted-file-types="image/jpeg, image/png"
-					:files="myFiles2"
+					v-bind:files="myFiles2"
+					:imagePreviewMaxHeight="512"
+					name="files"
+					server="/api/upload"
+					@updatefiles="updatefiles"
 				/>
 			</v-stepper-content>
 		</v-stepper-items>
 		<v-stepper-items>
 			<v-stepper-content step="3">
 				<FilePond
+					ref="fp3"
 					label-idle="拖放病理报告到此处..."
 					:allow-multiple="true"
 					accepted-file-types="image/jpeg, image/png"
 					:files="myFiles3"
+					:imagePreviewMaxHeight="512"
+					name="files"
+					server="/api/upload"
+					@updatefiles="updatefiles"
 				/>
 			</v-stepper-content>
 		</v-stepper-items>
 		<v-stepper-items>
 			<v-stepper-content step="4">
 				<FilePond
+					ref="fp4"
 					label-idle="拖放化验报告到此处..."
 					:allow-multiple="true"
 					accepted-file-types="image/jpeg, image/png"
 					:files="myFiles4"
+					:imagePreviewMaxHeight="512"
+					name="files"
+					server="/api/upload"
+					@updatefiles="updatefiles"
 				/>
 			</v-stepper-content>
 		</v-stepper-items>
 		<v-stepper-items>
 			<v-stepper-content step="5">
 				<FilePond
+					ref="fp5"
 					label-idle="拖放心电图文件到此处..."
 					:allow-multiple="true"
 					accepted-file-types="image/jpeg, image/png"
 					:files="myFiles5"
+					:imagePreviewMaxHeight="512"
+					name="files"
+					server="/api/upload"
+					@updatefiles="updatefiles"
 				/>
 			</v-stepper-content>
 		</v-stepper-items>
 		<v-stepper-items>
 			<v-stepper-content step="6">
+				<v-card max-width="400" class="mx-auto pa-4 text-center" flat>
+					<v-btn x-large color="primary" @click="gen">生成报告</v-btn>
+				</v-card>
 			</v-stepper-content>
 		</v-stepper-items>
+		<v-snackbar v-model="snackbar" color="blue lighten-1" top>无符合条件的记录</v-snackbar>
 	</v-stepper>
 </template>
 
@@ -81,6 +129,81 @@ export default {
 			myFiles3: [],
 			myFiles4: [],
 			myFiles5: [],
+			hasfile1: false,
+			hasfile2: false,
+			hasfile3: false,
+			hasfile4: false,
+			hasfile5: false,
+			pid: '',
+			pname: '',
+			psex: '',
+			pexamdate: '',
+			pedscp: [],
+			snackbar: false
+		}
+	},
+	methods: {
+		search() {
+			if (this.pid.length === 0) return;
+			this.$axios.get('/api/query',{params:{
+					pid: this.pid
+				}})
+				.then(response => {
+					const items = response.data;
+					if (items.length === 0) {
+						this.snackbar = true;
+					} else {
+						this.showItems(items);
+					}
+				})
+				.catch(error => {
+					console.dir(error);
+				});
+		},
+		showItems(items) {
+			this.pname = items[0].XM1;
+			this.psex = items[0].XB1;
+			this.pexamdate = items[0].ScheduleDate;
+			this.pedscp = items.map(v => v.SQMD);
+		},
+		clearall() {
+			this.pname = '';
+			this.psex = '';
+			this.pexamdate = '';
+			this.pedscp = [];
+		},
+		gen() {
+			let rptparams = {
+				id: this.pid,
+				name: this.pname,
+				sex: this.psex,
+				examdate: this.pexamdate,
+				edscp: this.pedscp,
+				files: []
+			};
+			for(let i = 0; i <5; ++i) {
+				const f = this.$refs['fp'+(i+1)].getFiles();
+//				f.forEach(v => console.log(v));
+				rptparams.files.push(f.map(v => ({id:JSON.parse(v.serverId)[0], ext:v.fileExtension}) ));
+			}
+//			console.log(rptparams);
+			this.$axios.post('/api/make', rptparams)
+				.then(response => {
+//					fileDownload(response.data, fileid + '_' + n + ".docx");
+					console.log(response.data);
+				})
+				.catch(error => {
+					if (error) {
+						console.dir(error);
+					}
+				});
+		},
+		updatefiles() {
+			this.hasfile1 = this.$refs['fp1'] && (this.$refs['fp1'].getFiles().length > 0);
+			this.hasfile2 = this.$refs['fp2'] && (this.$refs['fp2'].getFiles().length > 0);
+			this.hasfile3 = this.$refs['fp3'] && (this.$refs['fp3'].getFiles().length > 0);
+			this.hasfile4 = this.$refs['fp4'] && (this.$refs['fp4'].getFiles().length > 0);
+			this.hasfile5 = this.$refs['fp5'] && (this.$refs['fp5'].getFiles().length > 0);
 		}
 	},
 	components: {
@@ -88,3 +211,10 @@ export default {
     }
 }
 </script>
+
+<style>
+.tag-input span.v-chip {
+  background-color: #0000ef!important;
+  color: #fff!important;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	devServer: {
+		// open: process.platform === 'darwin',
+		// host: 'localhost',
+		open: false, //配置自动启动浏览器 
+		proxy: {
+			'/api': {
+					target: 'http://127.0.0.1:8361'
+			} 
+		}
+	},
+	transpileDependencies:['vuetify']
+}
